d6785651438d6f5dfee24600d6b64847d9bbdf77
Roberto Elizalde <devrced@outlook.com>
2021-10-09 19:00:29 -0500

    estructura de la aplicacion por casos de uso

---
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/session/login/Login.java
@@ -0,0 +1,16 @@
+package com.tt.ttpsmrpapp.usecases.session.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tt.ttpsmrpapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
