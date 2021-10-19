1c7e4842217bfd8c43be185fccff64164c329f4c
Roberto Elizalde <devrced@outlook.com>
2021-10-19 01:09:58 -0500

    Logic activity

--- app/src/main/java/com/tt/ttpsmrpapp/usecases/session/login/Login.java
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/session/login/Login.java
@@ -1,22 +1,37 @@
 package com.tt.ttpsmrpapp.usecases.session.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tt.ttpsmrpapp.R;
+import com.tt.ttpsmrpapp.network.api.body.LoginRequest;
+import com.tt.ttpsmrpapp.network.api.body.LoginResponse;
 import com.tt.ttpsmrpapp.usecases.session.signin.SignIn;
 
 public class Login extends AppCompatActivity {
+
     private TextView singIn;
+    private EditText email;
+    private EditText password;
+    private Button loginButton;
+
+    private LoginViewModel loginViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
         this.singIn = (TextView)findViewById(R.id.text_view_sing_in);
         this.singIn.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +39,22 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent singInIntent = new Intent(Login.this, SignIn.class);
                 startActivity(singInIntent);
+            }
+        });
+
+        this.email = (EditText)findViewById(R.id.editTextTextEmailAddress2);
+        this.password = (EditText)findViewById(R.id.editTextTextPassword2);
+
+        this.loginButton = (Button)findViewById(R.id.button3);
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginRequest loginRequest = new LoginRequest(email.getText().toString(), password.getText().toString());
+                LoginResponse loginResponse = loginViewModel.makeLoginRequest(loginRequest);
+
+                if (loginResponse != null)
+                    Toast.makeText(Login.this, loginResponse.getToken(), Toast.LENGTH_SHORT).show();
+
             }
         });
     }
