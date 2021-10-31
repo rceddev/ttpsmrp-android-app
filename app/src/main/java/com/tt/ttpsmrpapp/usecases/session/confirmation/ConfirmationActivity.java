f16dae6bc59453fca21a3b26d2827ecc36792a96
Roberto Elizalde <devrced@outlook.com>
2021-10-31 02:47:33 -0600

    se creo la activity para el codigo de confirmacion

---
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/session/confirmation/ConfirmationActivity.java
@@ -0,0 +1,16 @@
+package com.tt.ttpsmrpapp.usecases.session.confirmation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tt.ttpsmrpapp.R;
+
+public class ConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+    }
+}
