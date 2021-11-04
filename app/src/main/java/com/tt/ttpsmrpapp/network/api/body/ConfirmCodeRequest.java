61e95991bc7c0597d53419dd4dce9c0753b2b1a3
Roberto Elizalde <devrced@outlook.com>
2021-11-04 02:14:18 -0600

    Interfaz con constantes que indican el codigo de respuesta de las peticiones

---
+++ app/src/main/java/com/tt/ttpsmrpapp/network/api/body/ConfirmCodeRequest.java
@@ -0,0 +1,19 @@
+package com.tt.ttpsmrpapp.network.api.body;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ConfirmCodeRequest {
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
