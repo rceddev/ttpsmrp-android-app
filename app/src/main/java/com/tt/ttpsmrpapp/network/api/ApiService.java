e9801e427659a453ee150d723d83771e4d55e8f6
Roberto Elizalde <devrced@outlook.com>
2021-11-27 22:07:41 -0600

    se agrego el endpoint para obtener la lista de nodos centrales a la API

--- app/src/main/java/com/tt/ttpsmrpapp/network/api/ApiService.java
+++ app/src/main/java/com/tt/ttpsmrpapp/network/api/ApiService.java
@@ -1,10 +1,13 @@
 package com.tt.ttpsmrpapp.network.api;
 
+import com.tt.ttpsmrpapp.data.model.NodeCentral;
 import com.tt.ttpsmrpapp.network.api.body.ConfirmCodeRequest;
 import com.tt.ttpsmrpapp.network.api.body.LoginRequest;
 import com.tt.ttpsmrpapp.network.api.body.NodeCRegisterRequest;
 import com.tt.ttpsmrpapp.network.api.body.TokenResponse;
 import com.tt.ttpsmrpapp.network.api.body.DefaultResponse;
+
+import java.util.List;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -29,4 +32,7 @@ public interface ApiService {
 
     @POST("/api/nodo_central/register")
     Call<TokenResponse> registerCentralNode(@Body NodeCRegisterRequest registerNodoCRequest, @Header("authorization") String token);
+
+    @GET("/api/nodo_central/nodes")
+    Call<List<NodeCentral>> getCentralNodes(@Header("authorization") String token);
 }
