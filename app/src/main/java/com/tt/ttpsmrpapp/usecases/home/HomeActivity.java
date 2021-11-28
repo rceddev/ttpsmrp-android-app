ea6196b593c0e24407382dcaf2a94122b06728f7
Roberto Elizalde <devrced@outlook.com>
2021-11-27 22:09:08 -0600

    se agrego la logica para mostrar la lista de nodos centrales en la activity de home

--- app/src/main/java/com/tt/ttpsmrpapp/usecases/home/HomeActivity.java
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/home/HomeActivity.java
@@ -1,39 +1,65 @@
 package com.tt.ttpsmrpapp.usecases.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.tt.ttpsmrpapp.R;
 import com.tt.ttpsmrpapp.data.model.NodeCentral;
 import com.tt.ttpsmrpapp.usecases.home.adapters.NodeCentralAdapter;
 import com.tt.ttpsmrpapp.usecases.nodes.registration.NodeCRegistrationActivity;
+import com.tt.ttpsmrpapp.usecases.session.management.Session;
 
 import org.w3c.dom.Node;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private RecyclerView nodeCRecyclerView;
+    private HomeViewModel viewModel;
+    private Session session;
+    private ArrayList<NodeCentral> nodeCentrals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Session
+        this.session = new Session(this);
+
         //Find view on layout
         this.nodeCRecyclerView = findViewById(R.id.recycler_view_node_c_list);
 
-
         //Set adapter
-        NodeCentralAdapter adapter = new NodeCentralAdapter(getNodeCentrals());
+        this.nodeCentrals = new ArrayList<>();
+        NodeCentralAdapter adapter = new NodeCentralAdapter(nodeCentrals);
         this.nodeCRecyclerView.setAdapter(adapter);
         this.nodeCRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //ViewModel
+        this.viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        if (session.isLoggedIn()) {
+            this.viewModel.getNodeCentral(session.getToken()).observe(this, nodeCentrals -> {
+                Log.e("CentralNodesSize", String.valueOf(nodeCentrals.size()));
+                this.nodeCentrals.clear();
+                for (NodeCentral node : nodeCentrals){
+                    this.nodeCentrals.add(node);
+                }
+                adapter.notifyDataSetChanged();
+            } );
+        }else{
+            Log.e("Session", "Session not found, login it");
+        }
 
     }
 
@@ -42,30 +68,30 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private ArrayList<NodeCentral> getNodeCentrals(){
-        ArrayList<NodeCentral> nodeCentrals = new ArrayList<>();
-
-        NodeCentral node1 = new NodeCentral();
-        NodeCentral node2 = new NodeCentral();
-        NodeCentral node3 = new NodeCentral();
-
-        node1.setAlias("Jitomate");
-        node1.setScientificName("nombre cientifico del jitomate");
-        node1.setNodeName("Jardin");
-
-        node2.setAlias("Lechuga");
-        node2.setScientificName("nombre cientifico del Lechuga");
-        node2.setNodeName("Zotehuela");
-
-        node3.setAlias("Rabano");
-        node3.setScientificName("nombre cientifico del rabano");
-        node3.setNodeName("Azotea");
-
-        nodeCentrals.add(node1);
-        nodeCentrals.add(node2);
-        nodeCentrals.add(node3);
-
-        return nodeCentrals;
-
-    }
+//    private ArrayList<NodeCentral> getNodeCentrals(){
+//        ArrayList<NodeCentral> nodeCentrals = new ArrayList<>();
+//
+//        NodeCentral node1 = new NodeCentral();
+//        NodeCentral node2 = new NodeCentral();
+//        NodeCentral node3 = new NodeCentral();
+//
+//        node1.setAlias("Jitomate");
+//        node1.setScientificName("nombre cientifico del jitomate");
+//        node1.setNodeName("Jardin");
+//
+//        node2.setAlias("Lechuga");
+//        node2.setScientificName("nombre cientifico del Lechuga");
+//        node2.setNodeName("Zotehuela");
+//
+//        node3.setAlias("Rabano");
+//        node3.setScientificName("nombre cientifico del rabano");
+//        node3.setNodeName("Azotea");
+//
+//        nodeCentrals.add(node1);
+//        nodeCentrals.add(node2);
+//        nodeCentrals.add(node3);
+//
+//        return nodeCentrals;
+//
+//    }
 }
