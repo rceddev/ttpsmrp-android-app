542394ea2800b4265e1a8ca608afdacbf04b85f1
Roberto Elizalde <devrced@outlook.com>
2021-11-27 21:07:07 -0600

    cambio al hacer rebase

---
+++ app/src/main/java/com/tt/ttpsmrpapp/data/HomeRepository.java
@@ -0,0 +1,5 @@
+package com.tt.ttpsmrpapp.data;
+
+public class HomeRepository {
+
+}
--- app/src/main/java/com/tt/ttpsmrpapp/usecases/home/HomeViewModel.java
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/home/HomeViewModel.java
@@ -6,7 +6,11 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.tt.ttpsmrpapp.data.HomeRepository;
+
 public class HomeViewModel extends AndroidViewModel {
+
+    private HomeRepository respository;
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
