7dadb2569768335ab07e76e3b232b846c40045fa
Roberto Elizalde <devrced@outlook.com>
2021-12-06 20:45:12 -0600

    se agregaron las funciones al view model del nodo central y actualizacion de la actividad

--- app/src/main/java/com/tt/ttpsmrpapp/usecases/monitoring/NodeCentralViewModel.java
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/monitoring/NodeCentralViewModel.java
@@ -9,12 +9,18 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.tt.ttpsmrpapp.data.NodeRepository;
 import com.tt.ttpsmrpapp.data.model.Measurement;
+import com.tt.ttpsmrpapp.data.model.MeasurementV2;
+import com.tt.ttpsmrpapp.data.model.Plant;
 import com.tt.ttpsmrpapp.network.api.body.IdBluetooth;
+
+import java.util.List;
 
 public class NodeCentralViewModel extends AndroidViewModel {
 
     private NodeRepository repository;
     private MutableLiveData<Measurement> lastMeasurement;
+    private MutableLiveData<List<MeasurementV2>> lastMeasurementRange;
+    private MutableLiveData<Plant> plant;
 
     public NodeCentralViewModel(@NonNull Application application) {
         super(application);
@@ -33,5 +39,28 @@ public class NodeCentralViewModel extends AndroidViewModel {
         lastMeasurement = repository.getLastMeasurement(idBluetooth);
     }
 
+    public LiveData<List<MeasurementV2>> getLastMeasurementRange(String idBluetooth, int range){
+        if (lastMeasurementRange == null) {
+            lastMeasurementRange = new MutableLiveData<>();
+            refreshLastMeasurementRange(idBluetooth, range);
+        }
+        return lastMeasurementRange;
+    }
+
+    public void refreshLastMeasurementRange(String idBluetooth, int range){
+        lastMeasurementRange = repository.getLastMeasurementRange(idBluetooth, range);
+    }
+
+    public LiveData<Plant> getPlantById(int idPlant){
+        if (plant == null) {
+            plant = new MutableLiveData<>();
+            loadPlantInfo(idPlant);
+        }
+        return repository.getPlantById(idPlant);
+    }
+
+    public void loadPlantInfo(int idPlant){
+        plant = repository.getPlantById(idPlant);
+    }
 
 }
