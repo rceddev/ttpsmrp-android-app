1917ff997b34cd2efb776e04283b49533d914bf1
Roberto Elizalde <devrced@outlook.com>
2021-12-06 20:46:02 -0600

    se modificaron las clases de cada fragment para agregar la completa funcionlidad de la monitorizacion

--- app/src/main/java/com/tt/ttpsmrpapp/usecases/monitoring/fragments/MonitoringFragment.java
+++ app/src/main/java/com/tt/ttpsmrpapp/usecases/monitoring/fragments/MonitoringFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +41,7 @@ public class MonitoringFragment extends Fragment {
     private TextView lightValueTextView;
     private TextView phValueTextView;
     private ConstraintLayout phConstraintLayout;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     //ViewModel
     private NodeCentralViewModel viewModel;
@@ -87,7 +89,7 @@ public class MonitoringFragment extends Fragment {
         this.humidityValueTextView = (TextView) view.findViewById(R.id.text_view_humidity_value);
         this.lightValueTextView = (TextView) view.findViewById(R.id.text_view_light_value);
         this.phValueTextView = (TextView) view.findViewById(R.id.text_view_ph_value);
-
+        this.swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_monitoring);
         this.phConstraintLayout = (ConstraintLayout) view.findViewById(R.id.layout_card_ph);
 
         viewModel.getLastMeasurement(idBluetoothObj).observe(requireActivity(), measurement -> {
@@ -107,6 +109,34 @@ public class MonitoringFragment extends Fragment {
             if (measurement.getPh() != null){
                 phConstraintLayout.setVisibility(View.VISIBLE);
                 phValueTextView.setText(String.valueOf(measurement.getPh()));
+            }
+            swipeRefreshLayout.setRefreshing(false);
+        });
+
+        //Set refreshing listener
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //Refresh the data from the last measurement
+                viewModel.refreshLastMeasurement(idBluetoothObj);
+
+                /*If the data is exactly the same, it will never set refreshLayout state to false,
+                  so a tread is created to set refreshing state to false 4s later*/
+                Thread thread = new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            Thread.sleep(4000);
+                            if (swipeRefreshLayout.isRefreshing()){
+                                swipeRefreshLayout.setRefreshing(false);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                thread.start();
             }
         });
 
